4372adccf50393d86e99554b5f2f25f1f03b7da5
zuri227 <zuri22780@gmail.com>
2022-02-11 15:43:51 -0500

    Work on check board function

--- js/app.js
+++ js/app.js
@@ -55,7 +55,18 @@ const message = document.getElementById("message")
   
   //function checkBoard
   //Instead of a winning combos array, I want a function that checks the values of each circle to determine if there are 4 of playerOne or 4 of playerTwo in a winning combination. 
-    //checking rows would be simple, it would check for 4 in a row of either "1"s or "-1"s
+    // checking rows should be straightforward, it would check for 4 in a row of either "1"s or "-1"s. Basically i want it to iterate over each circle seeing 4 next to each other each hold a value of either 1 or -1. Need to decide whether I want to check each index against each other to determine if they are the same or whether it's easier to get a sum of 4 or -4 and then check to make sure they are next to each other somehow.
+      //check board row indexes 0-6, 7-13, 14-20, 21-27, 28-34, 35-41
+      
     //checking columns could also check for 4 in a column?
-    //checking diagonals would require math I think 
+      //check board column indexes [0][7][14][21][28][35], [1][8][15][22][29][36], [2][9][16][23][30][37], [3][10][17][24][31][38], [4][11][18][25][32][39], [5][12][19][26][33][40], [6][13][20][27][34][41]
+
+    //checking diagonals 
+      //check board diagonal indexes 
+
+
+  function checkBoard() {
+
+
+  }
 
