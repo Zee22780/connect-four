88202b43c15e58e6a771fa1ce1039f3ba5fc40dc
zuri227 <zuri22780@gmail.com>
2022-02-11 14:51:33 -0500

    Explore creating a checkBoard function

--- js/app.js
+++ js/app.js
@@ -20,22 +20,24 @@ const replayBtn = document.getElementById("replay=button")
 const message = document.getElementById("message")
 
 /*----------------------------- Event Listeners -----------------------------*/
-circles.forEach(circle => circle.addEventListener("click", handleClick))
-//adds a click event listener to each circle - the forEach method handles all 41 circles!
-replayBtn.addEventListener("click", init)
+//circles.forEach(circle => circle.addEventListener("click", handleClick))
+  //adds a click event listener to each circle - the forEach method handles all 41 circles!
+//replayBtn.addEventListener("click", init)
 
 /*-------------------------------- Functions --------------------------------*/
 //init function needs to:
-  //set all circles in board to null
-  //select playerOne to play
-    //assign 1 to playerOne 
-    //assign -1 to playerTwo
+  ////set all circles in board to null
+  ////select playerOne to play
+    ////assign 1 to playerOne 
+    ////assign -1 to playerTwo
   //hide replay button
-  //start "turn" variable as null
-  //assign null to winner variable
-  //call render function 
+  ////start "turn" variable as null
+  ////assign null to winner variable
+  //call render function
+  ////start counter at zero
+  //we need to limit the playerOne to only choosing from the bottom row
 
-  inti() {
+  function inti() {
     board = [
       null, null, null, null, null, null, null,
       null, null, null, null, null, null, null,
@@ -44,7 +46,16 @@ replayBtn.addEventListener("click", init)
       null, null, null, null, null, null, null,
       null, null, null, null, null, null, null,
     ]
+    playerOne = 1
+    playerTwo = -1
+    turn = null
+    winner = null
+    counter = 0
   }
   
-  
+  //function checkBoard
+  //Instead of a winning combos array, I want a function that checks the values of each circle to determine if there are 4 of playerOne or 4 of playerTwo in a winning combination. 
+    //checking rows would be simple, it would check for 4 in a row of either "1"s or "-1"s
+    //checking columns could also check for 4 in a column?
+    //checking diagonals would require math I think 
 
