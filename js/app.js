8f9bfcc35d35fb24947e4c59122dfaedda97486d
zuri227 <zuri22780@gmail.com>
2022-02-12 13:03:44 -0500

    Write winningCombos array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,75 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [0, 1, 2, 3],
+  [41, 40, 39, 38],
+  [7, 8, 9, 10],
+  [34, 33, 32, 31],
+  [14, 15, 16, 17],
+  [27, 26, 25, 24],
+  [21, 22, 23, 24],
+  [20, 19, 18, 17],
+  [28, 29, 30, 31],
+  [13, 12, 11, 10],
+  [35, 36, 37, 38],
+  [6, 5, 4, 3],
+  [0, 7, 14, 21],
+  [41, 34, 27, 20],
+  [1, 8, 15, 22],
+  [40, 33, 26, 19],
+  [2, 9, 16, 23],
+  [39, 32, 25, 18],
+  [3, 10, 17, 24],
+  [38, 31, 24, 17],
+  [4, 11, 18, 25],
+  [37, 30, 23, 16],
+  [5, 12, 19, 26],
+  [36, 29, 22, 15],
+  [6, 13, 20, 27],
+  [35, 28, 21, 14],
+  [0, 8, 16, 24],
+  [41, 33, 25, 17],
+  [7, 15, 23, 31],
+  [34, 26, 18, 10],
+  [14, 22, 30, 38],
+  [27, 19, 11, 3],
+  [35, 29, 23, 17],
+  [6, 12, 18, 24],
+  [28, 22, 16, 10],
+  [13, 19, 25, 31],
+  [21, 15, 9, 3],
+  [20, 26, 32, 38],
+  [36, 30, 24, 18],
+  [5, 11, 17, 23],
+  [37, 31, 25, 19],
+  [4, 10, 16, 22],
+  [2, 10, 18, 26],
+  [39, 31, 23, 15],
+  [1, 9, 17, 25],
+  [40, 32, 24, 16],
+  [9, 17, 25, 33],
+  [8, 16, 24, 32],
+  [11, 17, 23, 29],
+  [12, 18, 24, 30],
+  [1, 2, 3, 4],
+  [5, 4, 3, 2],
+  [8, 9, 10, 11],
+  [12, 11, 10, 9],
+  [15, 16, 17, 18],
+  [19, 18, 17, 16],
+  [22, 23, 24, 25],
+  [26, 25, 24, 23],
+  [29, 30, 31, 32],
+  [33, 32, 31, 30],
+  [36, 37, 38, 39],
+  [40, 39, 38, 37],
+  [7, 14, 21, 28],
+  [8, 15, 22, 29],
+  [9, 16, 23, 30],
+  [10, 17, 24, 31],
+  [11, 18, 25, 32],
+  [12, 19, 26, 33],
+  [13, 20, 27, 34],
+]
 
 
 
@@ -54,22 +124,13 @@ const message = document.getElementById("message")
   }
   
   //function checkBoard
-  //Instead of a winning combos array, I want a function that checks the values of each circle to determine if there are 4 of playerOne or 4 of playerTwo in a winning combination. 
-    // checking rows should be straightforward, it would check for 4 in a row of either "1"s or "-1"s. Basically i want it to iterate over each circle seeing 4 next to each other each hold a value of either 1 or -1. Need to decide whether I want to check each index against each other to determine if they are the same or whether it's easier to get a sum of 4 or -4 and then check to make sure they are next to each other somehow.
-      //check board row indexes 0-6, 7-13, 14-20, 21-27, 28-34, 35-41
-      
-    //checking columns could also check for 4 in a column?
-      //check board column indexes [0][7][14][21][28][35], [1][8][15][22][29][36], [2][9][16][23][30][37], [3][10][17][24][31][38], [4][11][18][25][32][39], [5][12][19][26][33][40], [6][13][20][27][34][41]
-
-    //checking diagonals 
-      //check board diagonal indexes 
+  //This function will check the board for winners
+    //uses the winningCombos array to look for winning combinations
+      //winning combos array is an array of arrays
 
 
-  function checkBoard() {
-    for(let i =0; i < board.length; i++){
-      const a = board[i][0]
-    }
+
+  // function checkBoard() {
   
-
-  }
+  // }
 
