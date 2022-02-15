d911fd08d3ef51023ed35b84b7286eda2dd4a07c
zuri227 <zuri22780@gmail.com>
2022-02-15 11:45:40 -0500

    Add music file

--- js/app.js
+++ js/app.js
@@ -76,21 +76,16 @@ const b = []
 const c = []
 const d = []
 
+const playMusic = new Audio("../music/Rush.mp3")
+
 /*---------------------------- Variables (state) ----------------------------*/
 let board, turn, winner, playerOne, playerTwo, counter
-
-//board is an array and will hold the values of each cell - all should start as null
-//turn will track which players turn it is by going back and forth from playerOne to playerTwo after each click
-//winner will track who the winner is and will start as null
-//playerOne and playerTwo will be our players and they will each hold a value of 1 and -1 respectively (these are the values that will be going into the board array as the game is played)
-//counter will count the number of clicks on the board. We need this for determining if there is a tie.
-
 
 /*------------------------ Cached Element References ------------------------*/
 const circles = document.querySelectorAll(".circle")
 const replayBtn = document.getElementById("replay-button")
 const message = document.getElementById("message")
-
+const musicBtn = document.getElementById("music")
 
 /*----------------------------- Event Listeners -----------------------------*/
 circles.forEach(circle => circle.addEventListener("click", handleClick))
